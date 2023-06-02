abee3364a0bdcd01729b455f3e23c93494631cef
Panagiotis <tsamou7@gmail.com>
2023-06-02 19:29:25 +0100

    form completed

---
+++ client/src/components/MetersForm.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+
+const MetersForm = ({creatInstance}) => {
+    const sizeInput =  '60px'
+
+    const [electric,setElectric] = useState(0)
+    const [gas,setGas] = useState(0)
+    const [oil,setOil] = useState(0)
+    const [mileage,setMileage] = useState(0)
+    const [flightUnder,setFlightUnder] = useState(0)
+    const [flightOver, setFlightOver] = useState(0)
+    const [newspaperRecycle, setNewspaperRecycle] = useState(false)
+    const [tinRecycle, setTinRecycle] = useState(false)
+
+    // all those handle  the change events of tthe form inputs
+    const handleGasChange = (event) =>{
+        setGas(event.target.value)
+    }
+    const handleElectricChange = (event) =>{
+        setElectric(event.target.value)
+    }
+    const handleOilChange = (event) =>{
+        setOil(event.target.value)
+    }
+    const handleMileageChange = (event) =>{
+        setMileage(event.target.value)
+    }
+    const handleFlightUnderChange = (event) =>{
+        setFlightUnder(event.target.value)
+    }
+    const handleFlightOverChange = (event) =>{
+        setFlightOver(event.target.value)
+    }
+    const handleNewspaperChange = (event) =>{
+        setNewspaperRecycle(event.target.checked)
+    }
+    const handleTinChange = (event) =>{
+        setTinRecycle(event.target.checked)
+    }
+
+    //handle the submit event, it sends all values to container to create an object
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        creatInstance(electric * 105, gas  * 105, oil * 113, mileage * 0.79, flightUnder * 1100, flightOver * 4400, flightOver * 1100 + flightUnder * 4400, newspaperRecycle * 184, tinRecycle * 166)
+    }
+
+    return (
+        <form onSubmit = {handleSubmit}>
+            <div>
+                <label htmlFor = "electric" >electric bill</label>
+                <input type = 'number' value = {electric} onChange = {handleElectricChange } id = "electric" style={{width: sizeInput}}/>
+                <label htmlFor = "gas" >gas bill</label>
+                <input type = "number" value = {gas} onChange = {handleGasChange} id = "gas" style={{width: sizeInput}}/>
+                <label htmlFor = "oil">oil bill</label>
+                <input type = "number" value = {oil} onChange = {handleOilChange} id = "oil"  style={{width: sizeInput}}/>
+            </div>
+            <div>
+                <label htmlFor = "mileage">mileage of on your car</label>
+                <input type = "number" value = {mileage} onChange = {handleMileageChange} id = "mileage"  style={{width: sizeInput}}/>
+                <label htmlFor = "flightUnder" >flights under 4 hours</label>
+                <input type = "number" value = {flightUnder} onChange = {handleFlightUnderChange} id = "flightUnder" style={{width: sizeInput}}/>
+                <label htmlFor = "flightOver" >flights over 4 hours</label>
+                <input type = "number" value = {flightOver} onChange = {handleFlightOverChange} id = "flightOver" style={{width: sizeInput}}/>
+            </div>
+            <div>
+                <label htmlFor = "newspaper">recycle newspaper</label>
+                <input type = "checkbox" id = "newspaper" onChange = {handleNewspaperChange} value = {newspaperRecycle} checked = {newspaperRecycle}/>
+                <label htmlFor = "tin" >recycle aluminum and tin</label>
+                <input type = "checkbox" id = "tin" onChange = {handleTinChange} value = {tinRecycle} checked = {tinRecycle}/>
+            </div>
+            <input type = "submit" value = "Submit" />
+        </form>
+    );
+};
+
+export default MetersForm
+
+
+// Multiply your monthly electric bill by 105
+// Multiply your monthly gas bill by 105
+// Multiply your monthly oil bill by 113
+// Multiply your total yearly mileage on your car by .79
+// Multiply the number of flights you’ve taken in the past year (4 hours or less) by 1,100
+// Multiply the number of flights you’ve taken in the past year (4 hours or more) by 4,400
+// Add 184 if you do NOT recycle newspaper
+// Add 166 if you do NOT recycle aluminum and tin
+// Add 1-8 together for your total carbon footprint
--- client/src/containers/container.js
+++ client/src/containers/container.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import MetersForm from "../components/MetersForm";
+
 function Container() {
+  const [newInstance, setNewInstance] = useState(null);
+
+  //console.log(newInstance)
+
+  // create a new object and store it in the newInstance var
+  const creatInstance = (electricValue, gasValue, oilValue, carValue, flightUnderValue, flightOverValue, flightValue, newspaperValue, tinValue) => {
+    const obj = {
+      electric: electricValue ,
+      gas: gasValue,
+      oil: oilValue,
+      car: carValue,
+      flightUnder: flightUnderValue,
+      flightOver: flightOverValue,
+      flightOverall: flightValue,
+      newspaperRecycle: newspaperValue,
+      tinRecycle: tinValue
+    }
+    setNewInstance(obj)
+  }
+
   return (
     <>
       <h1>Hello guys</h1>
+      <MetersForm creatInstance = {creatInstance}/>
     </>
   );
 }
