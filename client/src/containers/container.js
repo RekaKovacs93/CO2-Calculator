ed611ea7cc828af45459516f0978ec1e03b5083b
Reka <reka.kovacs1993@gmail.com>
2023-06-03 15:51:23 +0100

    chart added

--- client/src/containers/container.js
+++ client/src/containers/container.js
@@ -1,7 +1,12 @@
+import React from "react";
+import DoughnutChart from "../components/chart_component";
+
 function Container() {
+
   return (
     <>
       <h1>Hello guys</h1>
+      <DoughnutChart/>
     </>
   );
 }
