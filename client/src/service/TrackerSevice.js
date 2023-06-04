76dc4bc52f7865786d31fd5294c7d37e4fa4ff8d
Alex <a.ofori@rocketmail.com>
2023-06-04 14:40:46 +0100

    Updated TrackerService

--- client/src/service/TrackerSevice.js
+++ client/src/service/TrackerSevice.js
@@ -7,4 +7,22 @@ export const postTracker = (payload) => {
         headers: { 'Content-Type': 'application/json' }
     })
     .then(res => res.json())
-};
+}
+    
+    
+export const updateTracker = (payload) => {
+        return fetch(baseURL + booking._id, {
+          method: 'PUT',
+          body: JSON.stringify(booking),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(res => res.json());
+      }
+    
+export const deleteTracker = (id) => {
+        return fetch(baseURL + id, {
+          method: 'DELETE'
+        });
+    };
