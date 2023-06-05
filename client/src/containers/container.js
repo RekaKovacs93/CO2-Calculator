ae2a8877dc1555bd433b181f65339858792095d5
C <calums88@gmail.com>
2023-06-05 16:05:14 +0100

    fixed the update form

--- client/src/containers/container.js
+++ client/src/containers/container.js
@@ -10,9 +10,6 @@ function Container() {
   
   const [carbonTrackerCollection, setCarbonTrackerCollection] = useState(null)
   
-  // const [newInstance, setNewInstance] = useState(null);
-  console.log(carbonTrackerCollection)
-
   const addTrackingData = (data) => {
     setCarbonTrackerCollection(data)
   }
