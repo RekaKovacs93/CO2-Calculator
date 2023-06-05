da90aa383d47ca2355c4ee7d3b028ab1b3d501bf
Reka <reka.kovacs1993@gmail.com>
2023-06-05 16:08:52 +0100

    worked on labels

--- client/src/components/BarChart_component.js
+++ client/src/components/BarChart_component.js
@@ -4,6 +4,17 @@ import { Bar } from "react-chartjs-2";
 
 const BarChartComp = ({carbonTrackerCollection}) => {
 
+    const recommendationsForEmisions = {
+        electricBill: "Buy electric appliances with better energy efficiency and install solar panels",
+        gasBill: "Solar panels and home insulation will reduce your gas consumption",
+        oilBill: "You can replace oil with gas or other eco friendly energy source. Home insulation helps to reduce energy consumption, as well",
+        carMileage: "You can cycle for short distance and use bus or train for the long once",
+        flightUnder: "You can travel by bus or train  instead of airplain because the distance is't great.",
+        flightOver: "For long distance travels airplain  is the best solution, but you can try avoid meanningless travels",
+        recyclePaper: "You can start recycling paper.",
+        recycleAluminium: "You can start recycling aluminiun and tin packages"
+    }
+
     const data = {
 
         labels: ["Electricity", "Gas", "Petrol", "Car", "Flights", "Recycling"],
@@ -43,13 +54,34 @@ const BarChartComp = ({carbonTrackerCollection}) => {
         data,
         options: {
         indexAxis: 'y',
+        plugins: {
+            tooltip: {
+              callbacks: {
+                label: (context) => {
+                  const labelIndex = context.dataIndex;
+                  const datasetIndex = context.datasetIndex;
+                  const value = context.parsed.y;
+                  if (datasetIndex === 0) {
+                    // Custom label for "Your Score" dataset
+                    const recommendationKey = data.labels[labelIndex];
+                    const recommendation = recommendationsForEmisions[recommendationKey];
+                    return `Your Score: ${value} lbs\nRecommendation: ${recommendation}`;
+                  } else if (datasetIndex === 1) {
+                    // Custom label for "Average Score" dataset
+                    return `Average Score: ${value} lbs`;
+                  }
+                  return '';
+                },
+              },
+            },
+          },
         }
     };
 
 
     return (
         <>
-            <Bar data={data} />
+            <Bar data={data} options={config.options}/>
         </>
     )
     }
