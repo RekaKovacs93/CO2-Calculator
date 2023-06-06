5937fbfc15cb948937e73d6fc997e0e3aedaeb21
C <calums88@gmail.com>
2023-06-06 14:04:09 +0100

    working on homepage

--- client/src/containers/container.js
+++ client/src/containers/container.js
@@ -121,8 +121,8 @@ function Container() {
       <h1>We have to name this APP</h1>
       <Navbar/>
       <Routes>
-        <Route path="/" element=""/>
-        <Route path="/submit-form" element ={<MetersForm addTrackingData = {addTrackingData} monthsOfTheYear ={monthsOfTheYear}/>}/>
+        <Route path="/" element={<HomePage EmissionValues={carbonTrackerCollection }/>}/>
+        <Route path="/submit-form" element ={<MetersForm addTrackingData = {addTrackingData}/>}/>
         <Route path="/submit-form/:id" element={<SuccessfulSubmition/>}/>
         <Route path="/overview" element={<Overview carbonTrackerCollection = {carbonTrackerCollection} EmissionValues={carbonTrackerCollection}/>}/>
         <Route path="/display/:id" element={<DisplayInfo/>}/>
