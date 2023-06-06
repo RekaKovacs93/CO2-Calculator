e09350dfabfe4125b1c03de5ca8bda53c592e5c5
Reka <reka.kovacs1993@gmail.com>
2023-06-06 17:12:20 +0100

    merge

--- client/src/containers/container.js
+++ client/src/containers/container.js
@@ -24,61 +24,7 @@ import HomePage from "../components/HomePage";
 function Container() {
   const months = [  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   const years = [2016, 2017, 2018, 2019, 2016, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028]
-  const months = [  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  const years = [2016, 2017, 2018, 2019, 2016, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028]
   
-  const [carbonTrackerCollection, setCarbonTrackerCollection] = useState([
-  {
-    "_id": "647f1eb0a4d27a4c3c69e705",
-    "entries": {
-    "electricBill": 0,
-    "gasBill": 0,
-    "oilBill": 0,
-    "carMileage": 0,
-    "flightUnder": 560,
-    "flightOver": 10,
-    "recyclePaper": true,
-    "recycleAluminium": true
-    },
-    "emissions": {
-    "electricBill": 0,
-    "gasBill": 0,
-    "oilBill": 0,
-    "carMileage": 0,
-    "flightUnder": 616000,
-    "flightOver": 44000,
-    "recyclePaper": 0,
-    "recycleAluminium": 0
-    },
-    "totalEmissions": 660000
-  },
-  {
-    "_id": "647f1e40a4d27a4c3c69e704",
-    "entries": {
-    "electricBill": 0,
-    "gasBill": 0,
-    "oilBill": 0,
-    "carMileage": 0,
-    "flightUnder": 0,
-    "flightOver": 0,
-    "recyclePaper": false,
-    "recycleAluminium": false
-    },
-    "emissions": {
-    "electricBill": 0,
-    "gasBill": 0,
-    "oilBill": 0,
-    "carMileage": 0,
-    "flightUnder": 0,
-    "flightOver": 0,
-    "recyclePaper": 184,
-    "recycleAluminium": 166
-    },
-    "totalEmissions": 350
-    }
-
-  ])
-  const [monthsOfTheYear, setMonthsOfTheYear] = useState([])
   const [carbonTrackerCollection, setCarbonTrackerCollection] = useState([
   {
     "_id": "647f1eb0a4d27a4c3c69e705",
@@ -140,35 +86,6 @@ function Container() {
 
   }
 
-  useEffect (() => {
-    // getTwelve().then((data) => {
-    //   setLastTwelveItems(data)
-    // })
-    getMonthsAndYears()
-  }, [])
-  const getMonthsAndYears = () =>{
-    const allMonthsAndYears = years.map((year) => {
-      console.log(year)
-      const yearObj = {}
-      yearObj[year] = months
-      return yearObj
-      // return yearObj[`${year}`] = months
-    })
-    setMonthsOfTheYear(allMonthsAndYears)
-    console.log(monthsOfTheYear)
-  }
-  console.log(monthsOfTheYear)
-  
-
-  const updateTrackingData = (data, oldData) =>{
-  
-    const index = carbonTrackerCollection.map(elm => elm._id).indexOf(oldData._id)
-    const collectionLocal = carbonTrackerCollection
-    collectionLocal[index] = data
-    setCarbonTrackerCollection(collectionLocal)
-    console.log(carbonTrackerCollection)
-    console.log(index)
-  }
 
   useEffect (() => {
     // getTwelve().then((data) => {
