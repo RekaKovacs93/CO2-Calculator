46b62157e92a2d568d5d53099bf19835d9838eba
Reka <reka.kovacs1993@gmail.com>
2023-06-06 12:50:18 +0100

    sorry

--- client/src/containers/container.js
+++ client/src/containers/container.js
@@ -14,19 +14,16 @@ import SuccessfulSubmition from "../components/SuccesfullSubmition";
 import ErrorPage from "../components/ErrorPage";
 import DisplayInfo from "../components/DisplayInfo";
 import LineChart from "../components/LineChart";
-import EmissionsCard from "../components/EmissionsCard";
-import EmissionsGrid from "../components/EmissionsGrid";
-
 
 
 function Container() {
   
-  const [carbonTrackerCollection, setCarbonTrackerCollection] = useState([])
+  const [carbonTrackerCollection, setCarbonTrackerCollection] = useState(null)
   // const [lastTwelveItems, setLastTwelveItems] = useState([])
   
   console.log(carbonTrackerCollection)
   const addTrackingData = (data) => {
-    setCarbonTrackerCollection([...carbonTrackerCollection, data])
+    setCarbonTrackerCollection(data)
   }
 
   // useEffect (() => {
@@ -50,19 +47,14 @@ function Container() {
       </Routes>
       
       
-      <Navbar/>
-      
-      <MetersForm addTrackingData = {addTrackingData}/>
 
-      {/* {carbonTrackerCollection ? <DoughnutChart carbonTrackerCollection={carbonTrackerCollection}/>: null} */}
-      
-      {/* {carbonTrackerCollection ? <Suggestions carbonTrackerCollection={carbonTrackerCollection}/>: null} */}
-      {/* {carbonTrackerCollection ? <LineChart carbonTrackerCollection={carbonTrackerCollection}/>: null} */}
-      {/* {carbonTrackerCollection ? <BarChartComp carbonTrackerCollection={carbonTrackerCollection}/>: null} */}
-      {/* {carbonTrackerCollection ? <EmissionsCard carbonTrackerCollection={carbonTrackerCollection}/>: null} */}
-      {/* {carbonTrackerCollection ? <EmissionsGrid EmissionValues={carbonTrackerCollection}/>: null}  */}
-      {/* <EmissionsGrid emissions={emissions} removeBooking={removeBooking} checkIn={checkIn}/> */}
     </Router>
+      /* {carbonTrackerCollection ? <UpdateForm addTrackingData = {addTrackingData} carbonTrackerCollection={carbonTrackerCollection}/>: <MetersForm addTrackingData = {addTrackingData}/>}
+
+      {carbonTrackerCollection ? <DoughnutChart carbonTrackerCollection={carbonTrackerCollection}/>: null}
+      {/* {carbonTrackerCollection ? <Suggestions carbonTrackerCollection={carbonTrackerCollection}/>: null} */
+      // {carbonTrackerCollection ? <BarChartComp carbonTrackerCollection={carbonTrackerCollection}/>: null} 
+
   );
 }
 
