e548d880e4be28ccd84c29660bcaeb0089de80e7
C <calums88@gmail.com>
2023-06-06 15:00:07 +0100

    homepage functionality done

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,16 +1,24 @@
 import EmissionsGrid from "../components/EmissionsGrid";
 import EmissionsCard from "./EmissionsCard"
+import { Link } from 'react-router-dom'
+
 
 
 const HomePage = ({emission, removeEmission, EmissionValues}) => {
 
+  const limitedEmissionValues = EmissionValues.slice(0, 3);
 
   return (
     <>
-    <h1>Welcome User</h1>
-    <EmissionsGrid EmissionValues={EmissionValues}/>
+    <h1>Welcome</h1>
+    <EmissionsGrid EmissionValues={limitedEmissionValues}/>
     <EmissionsCard EmissionValues={EmissionValues} emission={emission} removeEmission={removeEmission}/>
-
+    <p>
+      Welcome to Our Carbon Footprint Calculator! We believe that understanding and reducing our carbon footprint is essential for a sustainable future. By raising awareness and making informed choices, we can mitigate climate change, take individual accountability, conserve the environment, improve our health, and inspire others to join us. Calculate your carbon footprint today and be a part of the journey towards a greener world!
+    </p>
+    <Link to='/overview'>
+      <button>See more information</button>
+    </Link>
     </>
   )
 }
