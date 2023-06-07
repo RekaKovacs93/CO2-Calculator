64380012f3c8a29b6852340828bfa7d8e3665122
C <calums88@gmail.com>
2023-06-07 19:32:08 +0100

    trying to merge main

--- client/src/components/AppFooter.js
+++ client/src/components/AppFooter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
 import './AppFooter.css';
-import forestImg from '../images/forest.jpeg'
 
 const { Footer } = Layout;
 
@@ -52,10 +51,8 @@ const AppFooter = () => {
           </ul>
         </div>
       </div>
-      <img className='footer-image' src={forestImg} alt="Forest"/>
       <p>© 2023</p>
     </Footer>
-    
   );
 };
 
